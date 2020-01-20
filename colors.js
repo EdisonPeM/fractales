14c6cd8188c8dfa692a2ed8eb367e6285122ed44
Edison Peñuela <edisonpe961206@hotmail.com>
2020-01-19 22:08:40 -0500

    agregando dinamismo en colores

--- colors.js
+++ colors.js
@@ -1,35 +1,60 @@
-//*
-var colors = []
-gama = new Array(256).fill(0)
-gama = gama.map((element, index) => {
-    return rgbToHex(index)
-})
+var gama = new Array(256).fill(0).map((e, index) => rgbToHex(index))
 
-for (let index = 0; index < gama.length; index += 16) {
-    let inverse_element = gama[gama.length - index - 1]
-    let element = gama[index]
-    colors.push("#" + element + "0000")
+var colors
+cambiar_colores()
+function cambiar_colores() {
+    colors = ["#000000"]
+
+    let first_tam = document.getElementById("first_tam").value
+    let second_tam = document.getElementById("second_tam").value
+    let third_tam = document.getElementById("third_tam").value
+    let fourth_tam = document.getElementById("fourth_tam").value
+    let fifth_tam = document.getElementById("fifth_tam").value
+
+    agregar(document.getElementById("first_color").value, first_tam == "" ? 16 : first_tam - 0)
+    agregar(document.getElementById("second_color").value, second_tam == "" ? 16 : second_tam - 0)
+    agregar(document.getElementById("third_color").value, third_tam == "" ? 16 : third_tam - 0)
+    agregar(document.getElementById("fourth_color").value, fourth_tam == "" ? 16 : fourth_tam - 0)
+    agregar(document.getElementById("fifth_color").value, fifth_tam == "" ? 16 : fifth_tam - 0)
 }
 
-for (let index = 0; index < gama.length / 2; index += 4) {
-    let inverse_element = gama[gama.length - index - 1]
-    let element = gama[index]
-    colors.push("#FF" + element + "00")
-}
-for (let index = gama.length / 2; index < gama.length; index++) {
-    let inverse_element = gama[gama.length - index - 1]
-    let element = gama[index]
-    colors.push("#FF" + element + "00")
+/**------- FUNCIONES -----------**/
+function agregar(color, cantidad = 16) {
+    if (color === "Rojo")
+        generic(cantidad, 1, 0, 0)
+    if (color === "Amarillo")
+        generic(cantidad, 1, 1, 0)
+    if (color === "Verde")
+        generic(cantidad, 0, 1, 0)
+    if (color === "Cyan")
+        generic(cantidad, 0, 1, 1)
+    if (color === "Azul")
+        generic(cantidad, 0, 0, 1)
+    if (color === "Violeta")
+        generic(cantidad, 1, 0, 1)
+    if (color === "Blanco")
+        generic(cantidad, 1, 1, 1)
 }
 
-for (let index = 0; index < gama.length; index++) {
-    let inverse_element = gama[gama.length - index - 1]
-    let element = gama[index]
-    colors.push("#FFFF" + element)
-}
-colors.push("#FFFFFF")
-//color = colors.reverse()
+function generic(cantidad, r, g, b) {
+    let base = colors[colors.length - 1]
+    let salto = parseInt(256 / cantidad)
+    if (256 % salto != 0) salto--;
+    let element_red, element_green, element_blue
+    for (let index = salto; index < gama.length; index += salto) {
+        if (r) element_red = parseInt(base[1]) < 2 ? gama[index] : base.substr(1, 2)
+        else element_red = parseInt(base[1]) < 2 ? base.substr(1, 2) : gama[gama.length - 1 - index]
 
+        if (g) element_green = parseInt(base[3]) < 2 ? gama[index] : base.substr(3, 2)
+        else element_green = parseInt(base[3]) < 2 ? base.substr(3, 2) : gama[gama.length - 1 - index]
+
+        if (b) element_blue = parseInt(base[5]) < 2 ? gama[index] : base.substr(5, 2)
+        else element_blue = parseInt(base[5]) < 2 ? base.substr(5, 2) : gama[gama.length - 1 - index]
+
+        colors.push("#" + element_red + element_green + element_blue)
+    }
+}
+/**------- FUNCION AUXILIAR -----------**/
 function rgbToHex(rgb) {
     var hex = Number(rgb).toString(16);
     if (hex.length < 2) {
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var miCanva = document.getElementById("miCanva");
 var ctx = miCanva.getContext("2d");
 
-var xmin, ymin, xmax, ymax, M, c, dx, dy;
+var xmin, ymin, xmax, ymax, c, dx, dy;
 var p = parseInt(miCanva.width)
 var q = parseInt(miCanva.height)
 var N = colors.length;
+var M = 100
 
 function dibujarJulia() {
-    xmin = -2
-    ymin = -2
-    xmax = 2
-    ymax = 2
+    xmin = -1.5
+    ymin = -1.5
+    xmax = 1.5
+    ymax = 1.5
 
-    M = 100
     dibujar(julia)
 }
 
@@ -22,8 +22,18 @@ function dibujarMandelbrot() {
     xmax = 1
     ymax = 1.75
 
-    M = 100
     dibujar(mandelbrot)
+
+    let x = (c[0] - xmin) / dx;
+    let x_0 = (- xmin) / dx;
+    let y = q - (c[1] - ymin) / dx;
+    let y_0 = (- ymin) / dx;
+
+    ctx.fillStyle = "white";
+    // ctx.fillRect(x_0, 0, 1, p);
+    // ctx.fillRect(0, y_0, q, 1);
+    ctx.fillRect(x, 0, 1, p);
+    ctx.fillRect(0, y, q, 1);
 }
 
 function dibujar(funcionFractal) {
@@ -31,34 +41,16 @@ function dibujar(funcionFractal) {
 
     for (let i = 0; i < p; i++) {
         for (let j = 0; j < q; j++) {
-
             n = funcionFractal(i, j);
             ctx.fillStyle = colors[n];
             ctx.fillRect(i, j, 1, 1);
         }
     }
-    /*
-    ctx.fillStyle = "white";
-    ctx.fillRect(p / 2, 0, 1, p);
-    ctx.fillRect(0, q / 2, 3 * q / 2, 1);
-    //*/
-}
-
-function xn_1(xn, yn) {
-    return Math.pow(xn, 2) - Math.pow(yn, 2) + c[0]
-}
-
-function yn_1(xn, yn) {
-    return 2 * xn * yn + c[1]
-}
-
-function rn(xn, yn) {
-    return Math.pow(xn, 2) + Math.pow(yn, 2)
 }
 
 function getValues() {
-    let a = parseFloat(document.getElementById("c_a").value)
-    let b = parseFloat(document.getElementById("c_b").value)
+    let a = document.getElementById("c_a").value - 0
+    let b = document.getElementById("c_b").value - 0
     if (isNaN(a)) a = 0
     if (isNaN(b)) b = 0
     c = [a, b]
@@ -67,24 +59,25 @@ function getValues() {
 }
 
 function dibujarColores() {
+    cambiar_colores()
     let N = colors.length
     let dy = q / N
-    for (let i = 0; i < N; i++) {
-        ctx.fillStyle = colors[i];
-        ctx.fillRect(0, i * dy, p, dy);
-        //ctx.strokeText(colors[i], 0, i * dy + dy);
+    for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < N; i++) {
+            ctx.fillStyle = colors[i];
+            ctx.fillRect(0, i * dy, p, dy);
+        }
     }
 }
 
 function julia(nx, ny) {
-    let xn1, yn1, r
     xn = xmin + nx * dx;
     yn = ymin + ny * dy;
     n = 0
 
     do {
-        xn1 = xn_1(xn, yn)
-        yn1 = yn_1(xn, yn)
+        xn1 = xn_1(xn, yn, c[0])
+        yn1 = yn_1(xn, yn, c[1])
         r = rn(xn1, yn1);
 
         n++;
@@ -97,15 +90,14 @@ function julia(nx, ny) {
 }
 
 function mandelbrot(nx, ny) {
-    dx = (xmax - xmin) / (p - 1);
     xn = xmin + nx * dx;
     yn = ymin + ny * dy;
     n = 0
     zx = 0
     zy = 0
     do {
-        zx1 = zx_1(zx, zy, xn)
-        zy1 = zy_1(zx, zy, yn)
+        zx1 = xn_1(zx, zy, xn)
+        zy1 = yn_1(zx, zy, yn)
         r = rn(zx1, zy1)
 
         n++;
@@ -117,10 +109,14 @@ function mandelbrot(nx, ny) {
     return n
 }
 
-function zx_1(xn, yn, x) {
+function xn_1(xn, yn, x) {
     return Math.pow(xn, 2) - Math.pow(yn, 2) + x
 }
 
-function zy_1(xn, yn, y) {
+function yn_1(xn, yn, y) {
     return 2 * xn * yn + y
+}
+
+function rn(xn, yn) {
+    return Math.pow(xn, 2) + Math.pow(yn, 2)
 }
