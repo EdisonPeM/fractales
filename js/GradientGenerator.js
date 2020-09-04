1105b582fb67e35db5519894068182bd9876a87e
Edison Peñuela <edisonpe961206@hotmail.com>
2020-09-04 17:23:48 -0500

    Fixed gradient controls flow

--- js/GradientGenerator.js
+++ js/GradientGenerator.js
@@ -1,70 +1,183 @@
 import colorControl from './ColorControl.js'
 
 class GradientGenerator {
+    mainElement = null;
+    gradientColors = [];
+    insertColor = false;
+
+    cacheGradient = [];
     initialGradient = [{
-            position: '10',
-            color: '#ff0000'
+            color: '#ff0000',
+            position: '10'
         },
         {
-            position: '40',
-            color: '#ffff00'
+            color: '#ffff00',
+            position: '40'
         },
         {
-            position: '70',
-            color: '#00ff77'
+            color: '#00ff77',
+            position: '70'
         }
     ]
 
     constructor(mainElement) {
         this.mainElement = mainElement;
-        this.initialGradient.forEach(obj => {
-            let tempObj = colorControl.createNewControl(obj);
-            this.mainElement.append(tempObj);
-        })
-
-        this.gradientColors = this.createGradientColors();
-        this.cacheColorsElements = [];
-        this.saveCache();
-
-        this.insertColor = false;
+        this.init();
 
         // Event Handler to Add New Color
         this.mainElement.addEventListener('click', (ev) => {
             if (this.insertColor) {
                 let cantColors = this.gradientColors.length;
 
-                let totalLenght = this.mainElement.getClientRects()[0].width;
+                let totalLenght = this.mainElement.clientWidth;
                 let newPosition = parseInt(ev.layerX * 100 / totalLenght);
 
-                let newElements = this.gradientColors.map(el => el.parentEl)
-                let indx = this.gradientColors.findIndex(el => el.positionEl.value > newPosition)
-                indx = (indx === -1) ? cantColors : indx;
+                let indx = this.gradientColors.findIndex(gc => gc.getPosition() > newPosition)
+                // indx = (indx === -1) ? cantColors : indx;
 
                 let color1 = '#000000';
                 let color2 = '#ffffff';
 
-                if ((indx === cantColors) && (cantColors > 0)) {
-                    color1 = this.gradientColors[cantColors - 1].getColor();
-                } else if ((indx === 0) && (cantColors > 0)) {
-                    color2 = this.gradientColors[0].getColor();
-                } else {
-                    color1 = this.gradientColors[indx - 1].getColor();
-                    color2 = this.gradientColors[indx].getColor();
+                if ((cantColors > 0)) {
+                    if (indx < 0) {
+                        color1 = this.gradientColors[cantColors - 1].getColor();
+                    } else if (indx === 0) {
+                        color2 = this.gradientColors[0].getColor()
+                    } else {
+                        color1 = this.gradientColors[indx - 1].getColor();
+                        color2 = this.gradientColors[indx].getColor();
+                    }
                 }
 
                 let newColor = colorControl.getIntermediateColor(color1, color2);
-                let newElement = colorControl.createNewControl({
+
+                this.addElement({
                     position: newPosition,
                     color: newColor
-                })
+                }, indx)
 
-                newElements.splice(indx, 0, newElement);
-                this.updateElements(newElements);
+                this.changeGradientBg();
 
                 this.mainElement.classList.remove('add-new');
                 this.insertColor = false;
             }
         })
+    }
+
+    init(colorsToGradient = this.initialGradient) {
+        this.cleanElements();
+        colorsToGradient.forEach(obj => {
+            this.addElement(obj)
+        })
+        this.changeGradientBg();
+        this.saveCache();
+    }
+
+    cleanElements() {
+        this.gradientColors = [];
+        this.mainElement.innerHTML = '';
+    }
+
+    addElement(obj, indx = -1) {
+        let colorEl = new colorControl(obj)
+
+        let newElement = colorEl.getElement();
+
+        if (this.gradientColors[indx]) {
+            let nextElement = this.gradientColors[indx].getElement();
+            this.mainElement.insertBefore(newElement, nextElement);
+            this.gradientColors.splice(indx, 0, colorEl)
+        } else {
+            this.mainElement.appendChild(newElement)
+            this.gradientColors.push(colorEl)
+        }
+
+        this.configListeners(colorEl);
+        // Update Element view
+        colorEl.changePosition();
+    }
+
+    configListeners(gc) {
+        gc.setDeleteClick(() => {
+            this.gradientColors = this.gradientColors.filter(els => els !== gc);
+            this.changeGradientBg();
+        });
+
+        gc.setPositionInput(() => {
+            this.updatePositionsLimits();
+            this.changeGradientBg();
+        });
+
+        gc.setValueInput(() => {
+            this.changeGradientBg();
+        });
+    }
+
+    changeGradientBg() {
+        let gcs = this.gradientColors;
+        this.mainElement.style.border = '1px solid #aaa';
+        this.mainElement.style.background = `
+        linear-gradient(to right, 
+            #000000 0%,
+            ${gcs.length > 0 ?
+                gcs.map(gc => `${gc.getColor()} ${gc.getPosition()}%`).join(",") + ','
+                : ''
+            }
+            #ffffff 100%
+        )`;
+    }
+
+    updatePositionsLimits() {
+        let minlimit = 0;
+        let maxlimit = 100;
+
+        this.gradientColors.forEach((gc, indx) => {
+            let hasNext = (indx + 1 !== this.gradientColors.length)
+
+            if (hasNext) {
+                let nextgc = this.gradientColors[indx + 1];
+                maxlimit = nextgc.getPosition();
+            } else {
+                maxlimit = 100;
+            }
+
+            gc.setPositionLimits(minlimit, maxlimit);
+            minlimit = gc.getPosition();
+        })
+    }
+
+    // ----------------------------------------------------
+    // ----------------------------------------------------
+    // ----------------------------------------------------
+    updateAllPositions() {
+        this.gradientColors.forEach(c => c.changePosition())
+    }
+
+    saveCache() {
+        this.cacheGradient = this.gradientColors.map(el => {
+            return {
+                color: el.getColor(),
+                position: el.getPosition()
+            }
+        })
+    }
+
+    addNewColor() {
+        this.insertColor = true;
+        this.mainElement.classList.add('add-new');
+    }
+
+    cancelNewColors() {
+        this.insertColor = false;
+        this.mainElement.classList.remove('add-new');
+
+        this.init(this.cacheGradient);
+    }
+
+    acceptChangedColors() {
+        this.insertColor = false;
+        this.mainElement.classList.remove('add-new');
+        this.saveCache();
     }
 
     createGradiant(size = 100) {
@@ -118,102 +231,29 @@ class GradientGenerator {
         return response
     }
 
-    createGradientColors() {
-        let elements = this.mainElement.getElementsByClassName('gradient-color');
-        let gcs = [...elements].map(color => {
-            let gc = new colorControl(color)
-            this.configListeners(gc);
-            return gc;
-        })
-
-        this.updatePositionsLimits(gcs);
-        this.setGradientBg(this.mainElement, gcs);
-
-        return gcs;
-    }
-
-    configListeners(gc) {
-        gc.setDeleteClick(() => {
-            this.mainElement.removeChild(gc.getParent());
-            this.gradientColors = this.createGradientColors();
-            this.setGradientBg(this.mainElement, this.gradientColors);
-        });
-
-        gc.setPositionInput(() => {
-            this.updatePositionsLimits(this.gradientColors);
-            this.setGradientBg(this.mainElement, this.gradientColors);
-        });
-
-        gc.setValueInput(() => {
-            gc.changeBg();
-            this.setGradientBg(this.mainElement, this.gradientColors);
-        });
-    }
-
-    updateElements(newElements) {
-        this.gradientColors.forEach(gc => {
-            this.mainElement.removeChild(gc.parentEl);
-        })
-        newElements.forEach(el => this.mainElement.appendChild(el))
-
-        this.gradientColors = this.createGradientColors();
-        this.updatePositionsLimits(this.gradientColors);
-        this.setGradientBg(this.mainElement, this.gradientColors);
-    }
-
-    updateAllPositions() {
-        this.gradientColors.forEach(c => c.changePosition())
-    }
-
-    updatePositionsLimits(gcs) {
-        let minlimit = 0;
-        let maxlimit = 100;
-        gcs.forEach((gc, indx) => {
-            let hasNext = (indx + 1 !== gcs.length)
-            if (hasNext) {
-                maxlimit = gcs[indx + 1].getPosition();
-            } else {
-                maxlimit = 100;
-            }
-
-            gc.setPositionLimits(minlimit, maxlimit);
-            minlimit = gc.getPosition();
-        })
-    }
-
-    saveCache() {
-        this.cacheColorsElements = this.gradientColors.map(gc => gc.parentEl);
-    }
-
-    addNewColor() {
-        this.insertColor = true;
-        this.mainElement.classList.add('add-new');
-    }
-
-    cancelNewColors() {
-        this.insertColor = false;
-        this.mainElement.classList.remove('add-new');
-        this.updateElements(this.cacheColorsElements);
-    }
-
-    acceptChangedColors() {
-        this.insertColor = false;
-        this.mainElement.classList.remove('add-new');
-        this.saveCache()
-    }
-
-    setGradientBg(element, gcs) {
-        element.style.border = '1px solid #aaa';
-        element.style.background = `
-        linear-gradient(to right, 
-            #000000 0%,
-            ${gcs.length > 0 ?
-                gcs.map(gc => `${gc.getColor()} ${gc.getPosition()}%`).join(",") + ','
-                : ''
-            }
-            #ffffff 100%
-        )`;
-    }
 }
 
 export default GradientGenerator;
+
+// createGradientColors(elements) {
+//     let gcs = elements.map(color => {
+//         let gc = new colorControl(color)
+//         this.configListeners(gc);
+//         return gc;
+//     })
+
+//     this.updatePositionsLimits(gcs);
+//     this.changeGradientBg(this.mainElement, gcs);
+
+//     return gcs;
+// }
+
+// updateElements(newElements) {
+//     this.cleanElements()
+
+//     newElements.forEach(el => this.mainElement.appendChild(el))
+
+//     this.gradientColors = this.createGradientColors();
+//     this.updatePositionsLimits(this.gradientColors);
+//     this.changeGradientBg();
+// }
